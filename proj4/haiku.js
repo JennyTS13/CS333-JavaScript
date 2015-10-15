5f516b371ce11b8a841155bb8fbb4728c2c1c47b
Tiffany Lam <tclam05@gmail.com>
2015-10-15 00:50:30 -0400

    added haiku

---
+++ proj4/haiku.js
@@ -0,0 +1,19 @@
+/* Authors: Tiffany Lam and Jenny Lin
+ * CS333 Project 4 
+ * Extension: Haiku
+ * A compilable and runnable haiku in JavaScript
+ * that helps demonstrate the function statement 
+ */
+
+
+/* shows how the function listNums can list the numbers in a list
+ * and the function can then be called */
+
+function listNums(list){						//5 syllables
+
+    for(num in list) { print( list[num] ); }	//7 syllables
+}
+
+listNums([7, 1]);								//5 syllables
+
+
