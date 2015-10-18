ae2a0bc014fd92ebc69fdaa8219b6012d0e1d4b2
Tiffany Lam <tclam05@gmail.com>
2015-10-18 00:53:53 -0400

    just in case - forgot

--- proj4/dataTypes.js
+++ proj4/dataTypes.js
@@ -16,8 +16,32 @@ print(typeof bool);		//boolean
 print(typeof obj);		//object
 print(typeof func);		//function
 
+print("\nScientific Notation");
 print(typeof (1.2));
 print(2.5e10);
 
-print(Number.MAX_VALUE);	//the max value in JavaScript
+print("\nMax and Min Integers");
+print(Math.pow(2, 53));
+print(Math.pow(2, 53) == Math.pow(2, 53) + 1);
 
+print(-Math.pow(2, 53));
+print(-Math.pow(2, 53) == -Math.pow(2, 53) - 1);
+
+print("\nInteger and Floating Point Precision");
+var x = 999999999999999;	//15 digits
+var y = 9999999999999999;	//16 digits
+print(x);					//999999999999999
+print(y);					//10000000000000000
+
+var i = 0.06;
+var j = 0.01;
+print("i =", i);
+print("j =", j);
+print("i + j =", i + j);	//0.06999999999999999, instead of 0.07
+
+print("\nNumber Properties");
+print(Number.MAX_VALUE);			//the max value in JavaScript
+print(Number.MIN_VALUE);			//the min value in JavaScript
+print(Number.NEGATIVE_INFINITY);	//represents negative infinity
+print(Number.NaN);					//represents a not-a-number value
+print(Number.POSITIVE_INFINITY);	//represents infinity
