d0f528e9476c721d7f152296e869d4c96b10bbe8
Tiffany Lam <tclam05@gmail.com>
2015-10-25 20:19:16 -0400

    updated haiku

--- proj5/haiku.js
+++ proj5/haiku.js
@@ -1,3 +1,10 @@
-var polymorph = ['test'];
-holds = ['hello', 3, 'world', 5, 'test'];
-print(polymorph, holds);
+/* Authors: Tiffany Lam and Jenny Lin
+ * CS333 Project 5
+ * Haiku
+ * A compilable and runnable haiku in JavaScript that helps
+ * demonstrate the concept of polymorphism
+ */
+
+var polymorph = ['test'];					//5 syllables
+holds = ['hello', 3, 'world', 5, 'test'];	//7 syllables
+print(polymorph, holds);					//5 syllables
