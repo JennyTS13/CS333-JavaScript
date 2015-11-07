de119b54ecbf448a9a868c8d282ba0a93180d21a
Jenny Lin <Jenny13TS13@gmail.com>
2015-11-06 23:40:58 -0500

    Simple I/O

---
+++ proj6/fileIO.js
@@ -0,0 +1,12 @@
+/* Authors: Tiffany Lam and Jenny Lin
+ * CS333 Project 6 
+ * Task 1: File I/O
+ * This program shows Javascript's built-in input/output capabilities
+ */
+
+var i = 1;
+var j = "Random String";
+
+print("\nCan only print to terminal");
+print("We can't use placeholders as they are not built in");
+print("But we can just use '+' to add the variable to printed line: " + i + j);
